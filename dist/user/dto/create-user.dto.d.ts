a54b2f7339b4defa3d1dff6a98a6b434109949bd
Mahendra Singh <126326125+Mahendra0115@users.noreply.github.com>
2026-05-11 18:18:56 +0530

    Delete dist directory

--- dist/user/dto/create-user.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserRole } from '../entities/user.entity';
-export declare class CreateUserDto {
-    email: string;
-    password: string;
-    fullName?: string;
-    role?: UserRole;
-}
--- dist/user/types/authenticated-user.type.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserRole } from '../entities/user.entity';
-export interface AuthenticatedUser {
-    id: number;
-    email: string;
-    role: UserRole;
-}
